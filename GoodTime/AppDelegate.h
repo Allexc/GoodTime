62ba2a59be1b0d056445bd1ce9b7eccb2827da95
Allen <Allen@zhangchuningdemacbook-pro.local>
2015-11-13 13:44:55 +0800

    Initial Commit

---
+++ GoodTime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoodTime
+//
+//  Created by Allen on 15/11/13.
+//  Copyright © 2015年 Allen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GoodTime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoodTime
+//
+//  Created by Allen on 15/11/13.
+//  Copyright © 2015年 Allen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
