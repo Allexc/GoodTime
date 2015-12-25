199e7742e41c4f3461992b23f4d254605022ffb0
ChenDongnan <huiwen@iosdeMacBook-Pro-2.local>
2015-12-25 08:52:23 +0800

    chen44444
    4444

---
+++ GoodTime/newCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  newCollectionViewCell.h
+//  GoodTime
+//
+//  Created by huiwen on 15/12/25.
+//  Copyright © 2015年 Allen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface newCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ GoodTime/newCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  newCollectionViewCell.m
+//  GoodTime
+//
+//  Created by huiwen on 15/12/25.
+//  Copyright © 2015年 Allen. All rights reserved.
+//
+
+#import "newCollectionViewCell.h"
+
+@implementation newCollectionViewCell
+
+@end
