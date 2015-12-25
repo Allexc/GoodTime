a8c15ba7a7ed39bcbb7b3e21f5a180cfcc48eec6
Allexc <184584890@qq.com>
2015-12-25 13:33:34 +0800

    一样一样一样一样

---
+++ GoodTime/NNNNNNNNN.h
@@ -0,0 +1,13 @@
+//
+//  NNNNNNNNN.h
+//  GoodTime
+//
+//  Created by Allen on 15/12/25.
+//  Copyright © 2015年 Allen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NNNNNNNNN : UITableViewCell
+
+@end
---
+++ GoodTime/NNNNNNNNN.m
@@ -0,0 +1,13 @@
+//
+//  NNNNNNNNN.m
+//  GoodTime
+//
+//  Created by Allen on 15/12/25.
+//  Copyright © 2015年 Allen. All rights reserved.
+//
+
+#import "NNNNNNNNN.h"
+
+@implementation NNNNNNNNN
+
+@end
